0a77311413b4f32c56845cbf3b5de9ccaf5e57f6
Tiruwork Abebe <tiru.tito16@gmail.com>
2022-04-07 15:42:54 -0700

    add js and index.html

---
+++ main.js
@@ -0,0 +1,65 @@
+// log a statement using console.log
+
+console.log("hello from main.js")
+
+// variables
+
+let age = 25
+ console.log(age)
+
+//  const salary = 0
+//  salary = 80000
+//  console.log(salary)
+
+// let sum =0
+// sum = 5
+// console.log(sum)
+
+// data types
+// const name = "javaScript"
+// const language = 'javaScript'
+// const channel = `codevoluation`
+
+// const total = 0 
+// const PI = 3.14 
+
+// const isPrimaryNumber =  true
+// const isNewUser = false 
+
+// let result
+// console.log(result)
+// const res = null
+// const data = null
+
+// // variable declaration
+
+// const person = {
+//     'firstName': 'Bruce',
+//     'lastName': 'Wayene',
+//     'age':   30
+// }
+// console.log(person.firstName)
+// const oddNumber = [1, 3, 5, 7, 9]
+// console.log(oddNumber[0])
+// console.log(oddNumber[2])
+// let a = 19
+// a = 'visgwas'
+// a = true
+// console.log(a) 
+
+// ASSIGNMENT OPERATOR
+
+let x = 10
+let y = 5
+console.log(x  % y)
+console.log(--y)
+console.log(++x)
+
+// comarsion operater
+const isValidNumber = x > 20 || 8 > y
+// comarsion operater
+// comarsion operater
+console.log(isValidNumber)
+
+const isvalid = true
+console.log(isvalid)
